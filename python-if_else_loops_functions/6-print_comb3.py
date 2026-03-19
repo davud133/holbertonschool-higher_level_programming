18d0cd4ac0aeb0eee9560375764c80b9bdfa7051
davud <davudgeroqiq@gmail.com>
2026-03-19 20:49:56 +0300

    new commit

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for i in range(0, 9):
     for d in range(i + 1, 10):
         if i == 8 and d == 9:
-            print("{comb}".format(comb = (str(i) + str(d))), end="\n")
+            print("{comb}".format(comb=(str(i) + str(d))), end="\n")
         else:
-            print("{comb}".format(comb = (str(i) + str(d))), end=", ")
-
+            print("{comb}".format(comb=(str(i) + str(d))), end=", ")
