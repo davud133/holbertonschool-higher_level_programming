0b0b335a768e70e60cf4dc84ccbbc6ab8e900752
davud <davudgeroqiq@gmail.com>
2026-03-19 21:56:54 +0300

    new commit

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+upper = False
+for i in range(122, 96, -1):
+    if upper:
+        i = i - 32
+        upper = False
+    else:
+        upper = True
+    print("{letter}".format(letter = chr(i)), end="")
