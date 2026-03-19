56d4447eda2838be8c4cb5a5bfee31cdc02bcb49
davud <davudgeroqiq@gmail.com>
2026-03-19 21:41:48 +0300

    new commit

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
-    tmp = a
+    result = 1
     if b < 0:
-        for i in range(1, b, -1):
-            a = a / tmp
+        for i in range(0, -b):
+            result = result / a
     elif b > 0:
-        for i in range(1, b):
-            a = a * tmp
-    return a
+        for i in range(0, b):
+            result = result * a
+    return result
