b22b0a18354c4075d86d3cfdd6f730fcae147b39
davud <davudgeroqiq@gmail.com>
2026-03-19 21:32:03 +0300

    new commit

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,7 +2,7 @@
 def pow(a, b):
     tmp = a
     if b < 0:
-        for i in range(0, b, -1):
+        for i in range(1, b, -1):
             a = a / tmp
     elif b > 0:
         for i in range(1, b):
