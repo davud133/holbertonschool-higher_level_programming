4f50f2b93315df8d9603bb038b2b6026abc0d72b
davud <davudgeroqiq@gmail.com>
2026-03-19 20:42:23 +0300

    new commit

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 99:
-        print("{number}, ".format(number = str(i).ljust(2,'0')), end="")
+        print("{number}, ".format(number = str(i).rjust(2,'0')), end="")
     else:
         print("99")
