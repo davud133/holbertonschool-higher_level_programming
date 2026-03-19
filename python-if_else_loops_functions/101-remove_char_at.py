dc8ae8b62da04472fec5c0315b621dac835790a7
davud <davudgeroqiq@gmail.com>
2026-03-19 22:00:46 +0300

    new commit

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     for i in range(0, len(str)):
-        if i = n:
+        if i == n:
             for d in range(n, len(str)-1):
                 str[d] = str[d+1]
