dd137d7a67d5a3c4002b5450f287fce79ef04836
davud <davudgeroqiq@gmail.com>
2026-03-21 00:10:22 +0300

    new commit

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_list[]
+    my_list = []
     for i in range(0, len(my_string)):
         if my_string[i] != 'c' and my my_string[i] != 'C':
             my_list[i] = my_string[i]
