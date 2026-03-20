c08084ff348ee7fdb2f6c135ecd059603f241246
davud <davudgeroqiq@gmail.com>
2026-03-21 00:05:38 +0300

    new commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > -1 and idx < len(my_list) - 1:
+    if idx > -1 and idx < len(my_list):
         new_list = my_list.copy()
         new_list[idx] = element
         return new_list
