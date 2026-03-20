0afd63790353d0f642d51303e9f54a5079994c1e
davud <davudgeroqiq@gmail.com>
2026-03-21 00:29:52 +0300

    new commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         for d in range(0, len(matrix[i])):
-            print("{:d}".format(matrix[i][d]), end=" " if d < len(matrix[i]) - 1 else "")
+            print("{:d}".format(matrix[i][d]), end=" "
+                  if d < len(matrix[i]) - 1 else "")
         print()
