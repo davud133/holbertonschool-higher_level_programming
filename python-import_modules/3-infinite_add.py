1f1c50f1477d4bc02390855f7b4c930c757e7f83
davud <davudgeroqiq@gmail.com>
2026-03-20 21:04:34 +0300

    new commit

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum1 = 0
+    for i in range(1, len(sys.argv)):
+        sum1 += int(sys.argv[i])
+    print("{sum1}".format(sum1=sum1))
