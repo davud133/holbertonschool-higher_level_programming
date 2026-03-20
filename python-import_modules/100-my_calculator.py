691d520ff1f71ac1a2c58363725598b4e3c0790f
davud <davudgeroqiq@gmail.com>
2026-03-20 23:18:22 +0300

    new commit

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1 as calc
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == '+':
+            print("{a} + {b} = {result}".format(a=a, b=b, result=a + b))
+        elif sys.argv[2] == '-':
+            print("{a} - {b} = {result}".format(a=a, b=b, result=a - b))
+        elif sys.argv[2] == '*':
+            print("{a} * {b} = {result}".format(a=a, b=b, result=a * b))
+        elif sys.argv[2] == '/':
+            print("{a} / {b} = {result}".format(a=a, b=b, result=a / b))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
