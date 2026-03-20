2b29899a51c8da5ddaca948dc8bf202be926eba0
davud <davudgeroqiq@gmail.com>
2026-03-20 20:52:00 +0300

    new commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    print("{arglen} argument".format(arglen = len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{i}: {arg}".format(i=i, arg=sys.argv[i]))
