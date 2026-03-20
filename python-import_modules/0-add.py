2daa7829f9f6d2b523d9d3b2c508b6f234c92fa4
davud <davudgeroqiq@gmail.com>
2026-03-20 20:37:12 +0300

    new commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     a = 1
     b = 2
-    from add_0.py import add
-    print("{}".format(add(a,b)))
+    from add_0 import add
+    print("{a} + {b} = {ans}".format(a = a, b = b, ans = add(a,b)))
