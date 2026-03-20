4a6e4321c4c45af48a82e981fb0ab36033b04b46
davud <davudgeroqiq@gmail.com>
2026-03-20 23:37:53 +0300

    new commit

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from magic_calculation_102 import add, sub
+    from magic_calculation_102 import add
+    from magic_calculation_102 import sub
 
 def magic_calculation(a, b):
     if a < b:
