7ff0caf59838483f92886040858e972336a058af
davud <davudgeroqiq@gmail.com>
2026-03-20 20:59:11 +0300

    new commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{arglen} argument:".format(arglen=len(sys.argv) - 1) if len(sys.argv) - 1 < 2 else "{arglen} arguments:".format(arglen=len(sys.argv)-1))
+    print("{arglen} arguments:".format(arglen=len(sys.argv) - 1) if len(sys.argv) - 1 > 1 else 
+          "{arglen} argument:".format(arglen=len(sys.argv)-1) if len(sys.argv) - 1 != 0 else 
+          "{arglen} arguments.".format(arglen=len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{i}: {arg}".format(i=i, arg=sys.argv[i]))
