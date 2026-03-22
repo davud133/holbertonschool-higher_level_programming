ac31c2246fb2d288a79e6dd89c2c1fe23b3b28f9
davud <davudgeroqiq@gmail.com>
2026-03-22 21:51:55 +0300

    new commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), len(sentence) > 0 sentence[0] : None) 
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None) 
