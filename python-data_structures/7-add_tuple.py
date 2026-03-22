ecb7ecbaf0e539c4359b58c1e26be005d57c9f71
davud <davudgeroqiq@gmail.com>
2026-03-22 21:48:02 +0300

    new commit

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,13 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = 0
     b = 0
-    a = tuple_a[0] + tuple_b[0]
-    b = tuple_a[1] + tuple_b[1]
+    if len(tuple_a) > 0:
+        a += tuple_a[0]
+    if len(tuple_a) > 1:
+        b += tuple_a[1]
+    if len(tuple_b) > 0:
+        a += tuple_b[0]
+    if len(tuple_b) > 1:
+        b += tuple_b[1]
     new_tuple = (a, b)
     return new_tuple
