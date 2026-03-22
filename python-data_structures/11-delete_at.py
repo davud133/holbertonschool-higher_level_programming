7f3ac230210a87c7be19b4be003badc49600903b
davud <davudgeroqiq@gmail.com>
2026-03-22 22:02:58 +0300

    new commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if idx >= len(my_list) or idx < 0:
         return my_list
-    new_list = [0] * len(my_list) - 1
+    new_list = [0] * (len(my_list) - 1)
     d = 0
     for i in range(0, len(my_list)):
         if i != idx:
