0611df36085b44779df822d80bb8c1049b70d675
davud <davudgeroqiq@gmail.com>
2026-03-24 00:51:11 +0300

    new commit

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-#
-#def multiply_by_2(a_dictionary):
-#    new_dict = a_dictionary.copy()
-#    for i, v in new_dict.items():
-#        new_dict[i] = new_dict[i] * 2
-#    return new_dict
-
 def multiply_by_2(a_dictionary):
-    new_dict = {x:a_dictionary[x] * 2 for x in a_dictionary.keys()}
+    new_dict = {x: a_dictionary[x] * 2 for x in a_dictionary.keys()}
     return new_dict
