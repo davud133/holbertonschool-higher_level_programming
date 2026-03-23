111dc3ac9b259be600f27c59da11c3eedd9c1114
davud <davudgeroqiq@gmail.com>
2026-03-24 02:01:49 +0300

    new commit

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    copy_d = a_dictionary.copy() 
+    copy_d = a_dictionary.copy()
     for key, val in copy_d.items():
         if val == value:
             del a_dictionary[key]
