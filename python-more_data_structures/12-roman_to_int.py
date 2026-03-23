2d1fc1775c6df6ed4e28d27b34bb780141350d6f
davud <davudgeroqiq@gmail.com>
2026-03-24 01:35:38 +0300

    new commit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    numbers = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = numbers[roman_string[0]]
     for i in range(1, len(roman_string)):
         if numbers[roman_string[i]] > numbers[roman_string[i - 1]]:
