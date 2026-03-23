afbef8698489e28d3bc0f6c068d146a0982b09dc
davud <davudgeroqiq@gmail.com>
2026-03-23 21:51:29 +0300

    new commit

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-a_dictionary = {'b':1, 'a':2, 'c':3}
 def print_sorted_dictionary(a_dictionary):
     sorted_key = sorted(a_dictionary)
-    new_dict = {x for x in sorted_key : a_dictionary[x]}
-    return new_dict
-print_sorted_dictionary(a_dictionary)
+    new_dict = {x:a_dictionary[x] for x in sorted_key}
+    print(new_dict)
