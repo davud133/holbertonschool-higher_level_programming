2e3df1fa46c4bab7560cf09a7d9d206ff516ca2b
Davud <davudgeroqiq@gmail.com>
2026-03-28 17:54:27 +0000

    new commit

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,11 +7,14 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             new_list.append(0)
+            continue
         except TypeError:
             print("wrong type")
             new_list.append(0)
+            continue
         except IndexError:
             print("out of range")
             new_list.append(0)
+            continue
         finally:
             print(new_list)
