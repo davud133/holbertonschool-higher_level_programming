bc1d10acee0300e23624fc5ac45af1017d2564da
Davud <davudgeroqiq@gmail.com>
2026-03-28 18:15:21 +0000

    new commit

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
     except TypeError as err:
-        print("Exception: " + str(err))
+        print("Exception: " + str(err), file=sys.stderr)
         return False
     except ValueError as err:
-        print("Exception " + str(err))
+        print("Exception " + str(err), file=sys.stderr)
         return False
     return True
