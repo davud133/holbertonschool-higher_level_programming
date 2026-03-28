2820dbf5f7a7b2a5ecc55bab9b4c5fb3e5578281
Davud <davudgeroqiq@gmail.com>
2026-03-28 19:14:06 +0000

    new commit

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -2,7 +2,9 @@
 def safe_function(fct, *args):
     import sys
     try:
-        return fct(args)
+        if args:
+            return fct(args)
+        return fct()
     except Exception as err:
         print("Exception: " + str(err), file=sys.stderr)
         return None
