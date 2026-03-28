43fa795b627207530fc92aeee6d5ac8cfa3bbc0f
Davud <davudgeroqiq@gmail.com>
2026-03-28 17:24:54 +0000

    new commit

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except TypeError, ValueError:
+    except TypeError:
+        return False
+    except ValueError:
         return False
     return True
