17b4de549ca8b60addcab7b96d04e1b63038d607
Davud <davudgeroqiq@gmail.com>
2026-03-28 17:28:30 +0000

    new commit

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i=0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        except TypeError:
+            continue
+    print()
+    return i
