31b7e1571279eab50e5098765438ee0b936dc021
Davud <davudgeroqiq@gmail.com>
2026-03-29 19:38:27 +0000

    new commit

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -11,11 +11,8 @@ def lazy_matrix_mul(m_a, m_b):
     except Exception as e:
         msg = str(e)
 
-        if
-            "ufunc 'multiply' did not contain a loop with signature matching types"
-            in msg:
-            raise
-            ValueError("Scalar operands are not allowed, use '*' instead")
+        if "ufunc 'multiply' did not contain a loop with signature matching types" in msg:
+            raise ValueError("Scalar operands are not allowed, use '*' instead")
 
         if "data type must provide an itemsize" in msg:
             raise TypeError("invalid data type for einsum")
