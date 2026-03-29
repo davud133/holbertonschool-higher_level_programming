dd914ec44f7c1b5b0f4536cccc6c423f3881dad0
Davud <davudgeroqiq@gmail.com>
2026-03-29 19:12:04 +0000

    new commit

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,11 +6,4 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices using NumPy."""
-    try:
-        return np.matmul(m_a, m_b)
-    except Exception as e:
-        msg = str(e)
-        if ("did not contain a loop with signature matching types" in msg or
-                "Scalar operands are not allowed" in msg):
-            raise ValueError("Scalar operands are not allowed, use '*' instead")
-        raise
+    return np.dot(m_a, m_b)
