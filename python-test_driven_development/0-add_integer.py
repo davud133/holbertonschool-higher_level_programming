8c101f204fc912e4fb92adc7b1e68347d8795ee7
Davud <davudgeroqiq@gmail.com>
2026-03-29 17:06:09 +0000

    new commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,6 +7,6 @@ def add_integer(a, b=98):
     """
     if not type(a) in [int, float]:
         raise TypeError("a must be an integer")
-    if not type(a) in [int, float]:
+    if not type(b) in [int, float]:
         raise typeError("b must be an integer")
     return int(a) + int(b)
