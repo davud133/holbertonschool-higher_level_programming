602ce8055cea3028010ddbe1a5692d55e7f783cb
Davud <davudgeroqiq@gmail.com>
2026-03-29 17:00:34 +0000

    new commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+        Returns the sum of 2 numbers
+    """
     if not (type(a) is in [int, float] and type(b) is in [int, float]):
         raise typeError("a must be an integer or b must be an integer")
     return int(a) + int(b)
