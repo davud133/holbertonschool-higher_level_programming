6eee18ed0aa7e55305993cc3cd3a4084054f1e8f
Davud <davudgeroqiq@gmail.com>
2026-03-29 19:05:28 +0000

    new commit

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Module that multiplies 2 matrices using NumPy"""
 
-
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiplies two matrices using NumPy"""
-    return np.matmul(m_a, m_b)
+    try:
+        return np.matmul(m_a, m_b)
+    except Exception:
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
