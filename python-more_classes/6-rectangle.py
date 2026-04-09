c3a999fc49bcab1af76d9ddcde2c10a7132aca21
Davud <davudgeroqiq@gmail.com>
2026-04-09 14:07:41 +0000

    new commit

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """defines  a Rectangle class"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initilizes the class"""
         if not isinstance(width, int):
