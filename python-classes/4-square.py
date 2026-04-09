20b9962241f24b3de3b2db799e76fc009568c733
Davud <davudgeroqiq@gmail.com>
2026-04-09 06:43:09 +0000

    new commit

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """defines a square class"""
-    
+
     def __init__(self, size=0):
         """initilizes the size variable"""
         if type(size) is not int:
@@ -12,10 +12,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     @property
     def size(self):
         """returns the size"""
         return self.__size
+
     @size.setter
     def size(self, value):
         """sets the value of size"""
@@ -24,6 +26,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """returns the area"""
         return self.__size ** 2
