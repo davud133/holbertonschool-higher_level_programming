3e1ec43788768bd9c707f6fd449ce74a6f3e494a
Davud <davudgeroqiq@gmail.com>
2026-04-12 15:13:46 +0000

    new commit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,6 +4,6 @@
 
 def is_same_class(obj, a_class):
     """checks if an object is instance of a class"""
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
