02fe3f52898f96ff37bcfc79d25cc33c7c00fc17
Davud <davudgeroqiq@gmail.com>
2026-04-12 15:49:59 +0000

    new commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a basegeometry class"""
+
+
+class BaseGeometry:
+    """defines a BaseGeometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
