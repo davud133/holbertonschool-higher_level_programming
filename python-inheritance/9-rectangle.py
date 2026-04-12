1f6742e6d7ee44f0f15b04bb54a997bcc402e3cb
Davud <davudgeroqiq@gmail.com>
2026-04-12 16:19:54 +0000

    new commit

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defines a Rectangle class inherited from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defines a Rectangle class inherited from BaseGeometry"""
+    def __init__(self, width, height):
+        """initilizes the width and height"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {width}/{height}".format(width=self.__width, height=self.__height)
