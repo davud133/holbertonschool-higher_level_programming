b0eb7cc1eff9b2fb720c31b591cc2b664cd91d37
Davud <davudgeroqiq@gmail.com>
2026-04-12 18:21:09 +0000

    new commit

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines CountedIterator class"""
+
+class CountedIterator:
+    """CountedIterator class"""
+    def __init__(self, obj):
+        self.iterator = iter(obj)
+        self.counter = 0
+    @property
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        return next(self.iter)
