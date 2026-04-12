19076bde73a96945eede4e6dc675a2a0333f0f08
Davud <davudgeroqiq@gmail.com>
2026-04-12 17:53:42 +0000

    new commit

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines VerboseList class"""
+
+
+from abc import ABC, abstractmethod
+
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print("Added [{item}] to the list.".format(item=item))
+    def extend(self, items):
+        super().extend(items)
+        print("Extended the list with [{x}] items.".format(x=len(items)))
+    def remove(self, item):
+        print("Removed [{item}] from the list.".format(item=item))
+        super().remove(item)
+    def pop(self, item=len(self) - 1):
+        print("Popped [{item}] from the list.".format(item=self[item]))
+        super().pop(item)
