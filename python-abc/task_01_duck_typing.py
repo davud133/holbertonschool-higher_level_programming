c5dae9a19a9988489e918a12a43107ce19711ddc
Davud <davudgeroqiq@gmail.com>
2026-04-12 17:37:22 +0000

    new commit

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""defines duck typing"""
+
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    """defines Shape class"""
+    @abstractmethod
+    def area(self):
+        """return the area"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """return the perimeter"""
+        pass
+
+class Circle(Shape):
+    """defines a Circle class"""
+    def __init__(self, radius):
+        """initilizes the radius variable"""
+        self.radius = radius
+
+    def area(self):
+        """return the area"""
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        """return the perimeter"""
+        return math.pi * self.radius * 2
+
+class Rectangle(Shape):
+    """defines a Rectangle class"""
+    def __init__(self, width, height):
+        """initilizes the width and height"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """return the area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """return the perimeter"""
+        return 2 * (self.width + self.height)
+
+def shape_info(obj):
+    """prints the area and the perimeter with the ducktyping"""
+    print(obj.area())
+    print(obj.perimeter())
