61a97411e49b12c598355555b643cddd37dd9d79
Davud <davudgeroqiq@gmail.com>
2026-04-12 15:28:34 +0000

    new commit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 defines a function that checks if
-an object is instance of a an 
+an object is instance of a an
 another class that inherited from another class
 """
 
 
 def inherits_from(obj, a_class):
     """
-    checks if an object is instance
-    of class that inherited from another class
+checks if an object is instance
+of class that inherited from another class
     """
     return isinstance(obj, a_class) and type(obj) is not  a_class
