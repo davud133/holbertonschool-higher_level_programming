77738699e47c407fad80427062b853728bcc2302
Davud <davudgeroqiq@gmail.com>
2026-04-12 17:15:49 +0000

    new commit

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defines ABC class"""
+
+from abc import ABC
+
+
+class Animal(ABC):
+    """defines Animal class inherited from ABC"""
+    @abstractmethod
+    def sound(self):
+        """returns the sound of an animal"""
+        pass
+
+class Dog(Animal):
+    """defines a Dog class"""
+    def sound(self):
+        """returns the sound of cat"""
+        return "Bark"
+
+class Cat(Animal):
+    """defines a Cat class"""
+    def sound(self):
+        """return soudn fo cat"""
+        return "Meow"
