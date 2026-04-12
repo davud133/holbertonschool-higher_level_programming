2e8d200e90080aeee12412f06480a10ab34b38b7
Davud <davudgeroqiq@gmail.com>
2026-04-12 16:30:08 +0000

    new commit

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines MyInt class inherited from int class"""
+
+
+class MyInt(int):
+    """defines MyInt class"""
+    def __eq__(self, other):
+        """checks if not equal"""
+        return self != other
+
+    def __ne__(self, other):
+        """checks if equal"""
+        return self == other
+
