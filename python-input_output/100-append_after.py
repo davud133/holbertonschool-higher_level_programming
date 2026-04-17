a024ad0dd7323699e08ba5af25b59a44fd5ff058
Davud <davudgeroqiq@gmail.com>
2026-04-17 21:04:01 +0000

    new commit

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""inserts a line of text to a file, after each line"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    data = ""
+    with open(filename, 'r', encoding="utf-8") as f:
+        data = f.read()
+
+    pos = data.find(search_string)
+    if pos == -1:
+        return "String Not Found"
+    pos = pos + len(search_string)
+    data = data[:pos] + new_string + data[pos:]
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(data)
+
