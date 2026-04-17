917f383d1feae5c84d53e696bfce7d1ba8c26b99
Davud <davudgeroqiq@gmail.com>
2026-04-17 21:47:24 +0000

    new commit

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -8,16 +8,16 @@ file_size = 0
 table = {"200": 0 , "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 line_c = 0
 if __name__ == "__main__":
-try:
-    for line in sys.stdin:
-        data = line.split()
-        file_size += int(data[-1])
-        table[str(data[-2])]+=1
-        line_c+=1
-        if line_c % 10 == 0:
-            print("File size: " + file_size)
-            for k, v in table.items():
-                print("{k}: {v}".format(k=k, v=v))
+    try:
+        for line in sys.stdin:
+            data = line.split()
+            file_size += int(data[-1])
+            table[str(data[-2])]+=1
+            line_c+=1
+            if line_c % 10 == 0:
+                print("File size: " + file_size)
+                for k, v in table.items():
+                    print("{k}: {v}".format(k=k, v=v))
     except KeyboardInterrupt:
         print("File size: " + file_size)
         for k, v in table.items():
