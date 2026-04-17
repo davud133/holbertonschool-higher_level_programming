401a412e59cc8222b154dd90430326fd256fa7ad
Davud <davudgeroqiq@gmail.com>
2026-04-17 20:04:21 +0000

    new commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ script that adds all arguments to a Python list, and then save them to a file"""
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
-import json
+
+import json, sys
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = []
 try:
-    my_list = load_from_json_file('add_item.json')
+    my_list = load('add_item.json')
 except Exception:
     my_list = []
 
