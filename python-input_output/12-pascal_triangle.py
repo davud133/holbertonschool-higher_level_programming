a6c06ef7272b8cb2215f71ed4c83ee64a5327e1d
Davud <davudgeroqiq@gmail.com>
2026-04-17 20:52:38 +0000

    new commit

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
+
+
+def pascal_triangle(n):
+    triangle = []
+    if n < 0:
+        return []
+    for x in range(n):
+        triangle.append([1])
+        for y in range(0, x):
+            if y == x - 1:
+                triangle[x].append(1)
+            else:
+                triangle[x].append(triangle[x - 1][y] + triangle[x - 1][y + 1])
+    return triangle
