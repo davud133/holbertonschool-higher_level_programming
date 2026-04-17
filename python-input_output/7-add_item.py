18b40fc223a86e09adc6846d93fcb822e50f327c
Davud <davudgeroqiq@gmail.com>
2026-04-17 20:05:09 +0000

    new commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" script that adds all arguments to a Python list, and then save them to a file"""
+""" script that adds arguments to file"""
 
 
-import json, sys
+import json
+import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
