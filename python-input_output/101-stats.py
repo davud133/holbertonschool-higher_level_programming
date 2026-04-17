16cccd594be4fd61ef95f442414eb62a514740c1
Davud <davudgeroqiq@gmail.com>
2026-04-17 21:46:41 +0000

    new commit

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -7,6 +7,7 @@ import sys
 file_size = 0
 table = {"200": 0 , "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 line_c = 0
+if __name__ == "__main__":
 try:
     for line in sys.stdin:
         data = line.split()
@@ -17,7 +18,7 @@ try:
             print("File size: " + file_size)
             for k, v in table.items():
                 print("{k}: {v}".format(k=k, v=v))
-except KeyboardInterrupt:
-    print("File size: " + file_size)
-    for k, v in table.items():
-        print("{k}: {v}".format(k=k, v=v))
+    except KeyboardInterrupt:
+        print("File size: " + file_size)
+        for k, v in table.items():
+            print("{k}: {v}".format(k=k, v=v))
