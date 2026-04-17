93fb11fb474cf614ce4ffd8b7a40f9b8bd0a0927
Davud <davudgeroqiq@gmail.com>
2026-04-17 19:58:55 +0000

    new commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,9 +4,11 @@
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 import sys
+import json
 
 my_list = []
 
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
-return my_list
+with open(add_item.json, 'w', encoding="utf-8") as f:
+    f.write(json.dumps(my_list))
