b0fcca6760e85763cec038319f905ece289fc6c6
Davud <davudgeroqiq@gmail.com>
2026-04-26 14:14:09 +0000

    new commit

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -12,17 +12,17 @@ class MyHandler(BaseHTTPRequestHandler):
             self.wfile.write(b'Hello, this is a simple API!')
         elif self.path =="/data":
             self.send_response(200)
-            self.send_header("content-type", "application/json")
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write(b'{"ame": "John", "age": 30, "city": "New York"}')
+            self.wfile.write(b'{"name": "John", "age": 30, "city": "New York"}')
         elif self.path == "/status":
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b'OK.')
+            self.wfile.write(b'OK')
         else:
             self.send_response(404)
             self.end_headers()
-            self.wfile.write(b'404 Page Not Found')
+            self.wfile.write(b'Endpoint not found')
 
 server = HTTPServer(("localhost",8000), MyHandler)
 server.serve_forever()
