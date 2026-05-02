0e9656ea4949b928d4c6a6152a48b753d0206253
Davud <davudgeroqiq@gmail.com>
2026-05-02 13:19:46 +0000

    new commit

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- avarages
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY value DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
