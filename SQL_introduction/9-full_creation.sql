7abcddc01ddb4b217f8934aa82f2d574a6ba69b1
Davud <davudgeroqiq@gmail.com>
2026-05-02 12:34:28 +0000

    new commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS second_table(
   score INT
 );
 INSERT INTO second_table
-values(1, "John", 10)
-values(2, "Alex", 3)
-values(3, "Bob", 14)
-values(4, "George", 8);
+values
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
