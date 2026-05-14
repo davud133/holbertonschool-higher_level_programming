cf268366616f9d4abeec42f0c335a4baee6b8a1a
Davud <davudgeroqiq@gmail.com>
2026-05-14 15:04:10 +0000

    new commit

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          )
     cursor = db.cursor()
     cursor.execute(
-        """SELECT name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC""", (sys.argv[4],)
+        """SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC""", (sys.argv[4],)
     )
     cities = cursor.fetchall()
     for city in cities:
