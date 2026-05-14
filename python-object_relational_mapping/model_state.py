98ef73b794e9a378b8130dc49ce0733350233b8e
Davud <davudgeroqiq@gmail.com>
2026-05-14 20:55:31 +0000

    new commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """contains the class definition of a State"""
+import sqlalchemy
+import sqlalchemy.ext.declerative
 
-if __name__ == "__main__":
-    import sqlalchemy
-    import sqlalchemy.ext.declerative
+Base = declerative_base()
 
-    Base = declerative_base()
-
-    class State(Base):
-        """State class"""
-        __tablename__ = "states"
-
-        id = Column(Integer, primary_key=True)
-        name = Column(String(128), nullable=False)
+class State(Base):
+    """State class"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
