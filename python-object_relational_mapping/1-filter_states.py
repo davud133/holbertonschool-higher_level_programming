98ff4643e4b99763a9997af82c04c7aaaa4775e2
Davud <davudgeroqiq@gmail.com>
2026-05-14 14:21:02 +0000

    new commit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                          )
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY states.id ASC"
+        "SELECT * FROM states"
+        WHERE BINARY name LIKE 'N%' ORDER BY states.id ASC"
     )
     states = cursor.fetchall()
 
