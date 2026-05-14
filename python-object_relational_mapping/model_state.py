84194b450dee3d3c06313691959294a0848ba6b4
Davud <davudgeroqiq@gmail.com>
2026-05-14 20:54:31 +0000

    new commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     Base = declerative_base()
 
     class State(Base):
+        """State class"""
         __tablename__ = "states"
 
-        id = column(Integer, primary_key=True)
-        name = column(String(128), nullable=False)
+        id = Column(Integer, primary_key=True)
+        name = Column(String(128), nullable=False)
