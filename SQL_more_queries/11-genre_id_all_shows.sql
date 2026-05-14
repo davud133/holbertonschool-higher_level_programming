c8fb5c07ed1a397646736e1eb5ae184447f8be1f
Davud <davudgeroqiq@gmail.com>
2026-05-14 08:35:55 +0000

    new commit

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- shows film
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, NULL) FROM tv_shows JOIN tv_show_genres ON tv_shows.id == tv_show_genres.show_id ORDER BY tv_shows.title , tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, NULL) FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title , tv_show_genres.genre_id ASC;
