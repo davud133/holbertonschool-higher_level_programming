ac05c829eedf2b634a5c95140f8e5d0dfc5df319
Davud <davudgeroqiq@gmail.com>
2026-05-14 10:19:47 +0000

    new commit

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,6 @@
--- Showss films
-SELECT tv_shows.title, NULL FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+-- Lists all shows without a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
