0d0e3200d88d5d13b6fd8cf8904a3356c61d38f0
Davud <davudgeroqiq@gmail.com>
2026-05-14 06:59:32 +0000

    new commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 --Creates user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED AS 'user_0d_1_pwd';
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
