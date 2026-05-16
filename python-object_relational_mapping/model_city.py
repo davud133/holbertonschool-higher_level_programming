8b8afddda4a02ec55d9ac82741568cf674186499
Davud <davudgeroqiq@gmail.com>
2026-05-16 19:29:48 +0000

    new commit

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,7 +2,7 @@
 """City"""
 
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
