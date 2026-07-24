9b69fc2000e2bb9a6ffeefbafe06fa7cd21c230c
Davud <davudgeroqiq@gmail.com>
2026-07-24 11:28:01 +0000

    new ocmmit

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (Number.isNaN(parseInt(argv[2], 10))) {
+  console.log('Not a number');
+} else {
+  console.log(parseInt(argv[2], 10));
+}
