f9d429947a8b54afdd101de088bec7876e299d7e
Davud <davudgeroqiq@gmail.com>
2026-07-24 14:24:30 +0000

    new commit

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+let first = 0; let second = 0;
+
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < argv.length; i++) {
+    if (parseInt(argv[i]) > first) {
+      second = first;
+      first = parseInt(argv[i]);
+    }
+  }
+  console.log(second);
+}
