bd89a06ad2939c1a0a61abf301ce430575241d7f
Davud <davudgeroqiq@gmail.com>
2026-07-24 13:51:21 +0000

    new commit

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const { argv } = require('node:process');
 if (Number.isNaN(parseInt(argv[2], 10))) {
   console.log('Not a number');
 } else {
-  console.log("My number: " + parseInt(argv[2], 10));
+  console.log('My number: ' + parseInt(argv[2], 10));
 }
