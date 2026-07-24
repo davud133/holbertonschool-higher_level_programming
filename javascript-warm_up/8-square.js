ebe09ec6d57299859d790d3ce932cf180d00a4eb
Davud <davudgeroqiq@gmail.com>
2026-07-24 14:07:20 +0000

    new commit

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('node:process')
+let n = parseInt(argv[2])
+
+if (Number.isNaN(n)){
+  console.log("Missing size");
+}
+else{
+  for (let i = 0; i < n; i++) {
+    console.log("X".repeat(n))
+  }
+}
